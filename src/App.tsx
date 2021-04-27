f2dded57d9ed01095de04a6a7e721dae79ea9298
Araozu <faraoz@unsa.edu.pe>
2021-04-27 10:03:33 -0500

    Add color bar animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, Switch, Match, createSignal, createEffect, createMemo } from "solid-js";
+import { lazy, Suspense, Switch, Match, createSignal, createEffect } from "solid-js";
 import { useRouter } from "./Router";
 import Index from "./Pages/Index";
 import { Header } from "./Header";
@@ -7,7 +7,7 @@ import { LoadingScreen, LoadingScreenStatus } from "./LoadingScreen";
 
 const time = (t: number) => new Promise(resolve => {
     setTimeout(resolve, t)
-})
+});
 
 const Learn = lazy(async () => {
     await time(2000)
@@ -36,9 +36,58 @@ function Separator() {
         }
     });
 
+    const [position, setPosition] = createSignal(0);
+    const [animationActive, setAnimationActive] = createSignal(false);
+
+    const animate = async () => {
+        setAnimationActive(true);
+        const tick = 30;
+        let pos = position();
+
+        let variance = 0.06;
+
+        // accelerate
+        while (true) {
+            if (pos >= 50) pos = 0;
+
+            setPosition(pos + variance);
+            if (variance < 1) {
+                variance *= 1.5;
+            } else {
+                variance = 1;
+            }
+            pos += variance;
+            await time(tick);
+            if (!animationActive()) break;
+        }
+
+        // deaccelerate
+        while (variance > 0.09) {
+            if (pos >= 50) pos = 0;
+
+            setPosition(pos + variance);
+            variance /= 1.5;
+            pos += variance;
+            await time(tick);
+        }
+
+        return;
+    }
+
+    const animateHelper = () => {
+        if (animationActive()) {
+            setAnimationActive(false);
+        } else {
+            animate();
+        }
+    }
+
     return (
-        <div className={css(e.container)}>
-            <div className={css(e.separator) + " rainbow-separator"}/>
+        <div className={css(e.container)} onClick={animateHelper}>
+            <div
+                className={css(e.separator) + " rainbow-separator"}
+                style={{transform: `translateX(-${position()}%)`}}
+            />
         </div>
     )
 }
